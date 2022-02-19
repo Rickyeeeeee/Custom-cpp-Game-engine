570d9c56ffa1e9cd9766995d78952da507630488
Ricky Hsu <rickyhsu2017@gmail.com>
2022-02-19 16:16:16 +0800

    clean

--- build/CMakeFiles/Coolgame.dir/src/State/state.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Coolgame.dir/src/State/state.cpp.obj: \
- C:\Users\ricky\Dev\coolgame\src\State\state.cpp \
- C:\Users\ricky\Dev\coolgame\src\State\state.h
--- build/CMakeFiles/Coolgame.dir/src/state.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Coolgame.dir/src/state.cpp.obj: \
- C:\Users\ricky\Dev\coolgame\src\state.cpp \
- C:\Users\ricky\Dev\coolgame\src\state.h
